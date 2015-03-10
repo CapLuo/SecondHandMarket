ebcadc3c0f2cea0e06e6421cdc09048a30b6bfa4
CapLuo <331701335@qq.com>
2015-03-10 16:39:34 +0800

    add write sdcard permison.

--- src/com/secondhand/util/FieldUtil.java
+++ src/com/secondhand/util/FieldUtil.java
@@ -3,6 +3,7 @@ package com.secondhand.util;
 public class FieldUtil {
 
 	public static final String TAG = "Flea street";
+	public static final String AppFile = "fleastreet";
 
 	public static final String SHARE_NAME = "flea_street";
 	public static final String GOODS_BUSINESS = "goods_business"; // 物品交易通知
@@ -12,4 +13,5 @@ public class FieldUtil {
 	public static final String CUSTOM_LABLE_WORD = "custom_lable_word";// 自订关键词
 
 	public static final String Link_API = "http://fleastreet.chinacloudsites.cn/api";
+
 }
